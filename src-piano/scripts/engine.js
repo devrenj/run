8842893d0bf3686bff17b3af211ae2dcc9097026
devrenj <devrenjbr@gmail.com>
2023-11-23 04:31:30 -0300

    Funcionalidades com sons

--- src-piano/scripts/engine.js
+++ src-piano/scripts/engine.js
@@ -1,15 +1,21 @@
 const pianoKeys = document.querySelectorAll('.piano-keys .key');
 
-let audio = new Audio('./src/tines/a.wav');
+let audio = new Audio('./src-piano/tunes/a.wav');
 
 const playTune = (key)=> {
-  audio.src = `src/tunes/${key}.wav`
+  audio.src = `./src-piano/tunes/${key}.wav`;
   audio.play();
+
+  const clickedKey = document.querySelector(`[data-key="${key}"]`)
+  clickedKey.classList.add('active')
+  setTimeout(()=> {
+    clickedKey.classList.remove('active')
+  },150);
 };
 
 pianoKeys.forEach((key)=> {
   console.log(key.dataset.key);
-  key.addEventListener('click', ()=> playTune(key.dataset));
+  key.addEventListener('click', ()=> playTune(key.dataset.key));
 })
 
 document.addEventListener('keydown', (e)=> {
