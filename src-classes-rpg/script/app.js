aabfc2537014db231c5a3f42ac8f0e2fcc9d388d
devrenj <devrenjbr@gmail.com>
2024-06-30 23:16:13 -0300

    Manipulando classes

--- src-classes-rpg/script/app.js
+++ src-classes-rpg/script/app.js
@@ -4,22 +4,38 @@ const { Monge } = require('./Classes/monge');
 const { Feiticeiro } = require('./Classes/feiticeiro');
 const { Espada } = require('./Armas/Espadas/espadas');
 
-const espada_de_treino = new Espada(
-  'espada de treino',
+function criarItem(item) {
+  const espada_de_treino = new item(
+    'espada de treino',
   'madeira',
   'treino',
   'Guerreiro',
   'fisico',
   1,
   0
-);
+  );
+}
+// criarItem(Espada)
 
-const monge1 = new Monge('Tayla', 20, 'm', 'artes marciais');
-const feiticeiro1 = new Feiticeiro('Midnighter', 120, 'x', 'cajado');
-const ninja1 = new Ninja('Mata Hari', 31, 'f', 'shuriken');
-const guerreiro1 = new Guerreiro('Furiosa', 18, 'f', espada_de_treino);
+function criarHerois(variavel, classe, nome, level, genero, arma) {
+  const variavel = new classe(nome, level, genero, arma);
+  // const monge1 = new Monge('Tayla', 20, 'm', 'artes marciais');
+  // const monge2 = new classe(nome, level, genero, arma)
+  // const feiticeiro1 = new Feiticeiro('Midnighter', 120, 'x', 'cajado');
+  // const ninja1 = new Ninja('Mata Hari', 31, 'f', 'shuriken');
+  // const guerreiro1 = new Guerreiro('Furiosa', 18, 'f', espada_de_treino);
+}
+criarHerois(monge1, Monge, 'Tayla', 20, 'm', 'artes marciais');
 
-ninja1.atacar();
-feiticeiro1.atacar();
-monge1.atacar();
-guerreiro1.atacar();
+function ataques() {
+  ninja1.atacar();
+  feiticeiro1.atacar();
+  monge1.atacar();
+  guerreiro1.atacar();
+}
+
+function init() {
+  console.log('olá!');
+}
+
+init();
