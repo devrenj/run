c8762876a8d1c23b5e7748795ab957595a8ee844
Betoe <devrenjbr@gmail.com>
2024-11-11 01:29:41 -0300

    fix: Caminho para arquivos e tecla pressionada não interromper outra

--- piano-src/scripts/engine.js
+++ piano-src/scripts/engine.js
@@ -1,23 +1,46 @@
-const pianoKeys = document.querySelectorAll('.piano-keys .key');
+document.addEventListener('DOMContentLoaded', () => {
 
-let audio = new Audio('./src-piano/tunes/a.wav');
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck = document.querySelector(".keys-check input");
 
-const playTune = (key)=> {
-  audio.src = `./src-piano/tunes/${key}.wav`;
-  audio.play();
+let mapedKeys = [];
+let volume = 0.5;
 
-  const clickedKey = document.querySelector(`[data-key="${key}"]`)
-  clickedKey.classList.add('active')
-  setTimeout(()=> {
-    clickedKey.classList.remove('active')
-  },150);
+
+const playTune = (key) => {
+  const audio = new Audio("./piano-src/tunes/a.wav");
+  audio.volume = volume;
+  audio.src = `./piano-src/tunes/${key}.wav`;
+	audio.play();
+
+	const clickedKey = document.querySelector(`[data-key="${key}"]`)
+	clickedKey.classList.add("active");
+	setTimeout(() => {
+		clickedKey.classList.remove("active");
+	},150)
 };
 
-pianoKeys.forEach((key)=> {
-  console.log(key.dataset.key);
-  key.addEventListener('click', ()=> playTune(key.dataset.key));
+pianoKeys.forEach((key) => {
+	key.addEventListener("click", () => playTune(key.dataset.key));
+	mapedKeys.push(key.dataset.key);
 })
 
-document.addEventListener('keydown', (e)=> {
-  playTune(e.key)
+document.addEventListener("keydown", (e) => {
+	if (mapedKeys.includes(e.key)) {
+		playTune(e.key);
+	}
 })
+
+const handleVolume = (e) => {
+	volume = e.target.value;
+}
+
+const showHideKeys = () => {
+	pianoKeys.forEach(key => key.classList.toggle("hide"))
+}
+
+volumeSlider.addEventListener("input", handleVolume);
+
+keysCheck.addEventListener("click", showHideKeys);
+});
